8e0e4f3ef27bc5c6b469b52dccbbabc68b62db40
SainTool <78474049+SainTool@users.noreply.github.com>
2024-09-04 13:44:52 +0700

    Add unity scripts folder

---
+++ Scripts/CollisionWithBox.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionWithBox : MonoBehaviour
+{
+    public float newSpeed;
+    public Player moveScript;
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            moveScript.CollisionBox(newSpeed);
+        }
+    }
+}
+
---
+++ Scripts/SwitchButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchButton : MonoBehaviour
+{
+    public Door lockDoor;
+    private Animator anim;
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetBool("On", false);
+    }
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" || collision.tag == "Box")
+        {
+            anim.SetBool("On", true);
+            lockDoor.lockDoor(false);
+        }
+
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" || collision.tag == "Box")
+        {
+            anim.SetBool("On", false);
+            lockDoor.lockDoor(true);
+        }
+    }
+}
